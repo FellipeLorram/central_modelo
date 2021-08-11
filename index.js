b6cfac63c1a5ec7c40da8ca7a964d5ad1d4b8556
fellipe lorram <fellipelorram@gmail.com>
2021-08-11 09:13:43 -0300

    mundanças na responsividade

--- index.js
+++ index.js
@@ -6,4 +6,32 @@ toggleSideBar.addEventListener('click', () => {
     main.classList.toggle('active');
 });
 
+const open_with = document.querySelector('.open_with');
+const open_with_left_side = document.querySelector('.open_with_left_side');
+
+open_with.addEventListener('click', e => {
+    if (window.screen.width <= 768) {
+        e.target.parentElement.style.top = "70%";
+        e.target.style.display = "none";
+        open_with_left_side.style.display = "block";
+    } else {
+        e.target.parentElement.style.left = "calc(100vw - 25%)";
+        e.target.style.display = "none";
+        open_with_left_side.style.display = "block";
+    }
+});
+
+open_with_left_side.addEventListener('click', e => {
+
+    if (window.screen.width <= 768) {
+        e.target.parentElement.style.top = "10%";
+        e.target.style.display = "none";
+        open_with.style.display = "block";
+    } else {
+        e.target.parentElement.style.left = "35px";
+        e.target.style.display = "none";
+        open_with.style.display = "block";
+    }
+
+});
 
