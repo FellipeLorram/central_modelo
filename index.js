7424aca5ee63644ff023801b3ed2eb8c0989d521
FellipeLorram <83411249+FellipeLorram@users.noreply.github.com>
2021-08-20 12:22:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import novoLembrete from './novoLembrete.js'
+
 const toggleSideBar = document.querySelector('.top_bar_toggle');
 const main = document.querySelector('.main');
 const navigation = document.querySelector('.navigation');
@@ -8,6 +10,12 @@ toggleSideBar.addEventListener('click', () => {
 
 const open_with = document.querySelector('.open_with');
 const open_with_left_side = document.querySelector('.open_with_left_side');
+const add_new_note_content = document.querySelector('.add_new_note_content');
+
+add_new_note_content.addEventListener('click', e => {
+    if (e.target !== open_with_left_side && e.target !== open_with) novoLembrete.open()
+
+});
 
 open_with.addEventListener('click', e => {
     if (window.screen.width <= 768) {
@@ -36,15 +44,15 @@ open_with_left_side.addEventListener('click', e => {
 });
 
 // Novo Lembrete
-const addNewPacienteMobile = document.querySelector('.add_new_lembrete_content_mobile');
+const addNewLembretMobile = document.querySelector('.add_new_lembrete_content_mobile');
 
-addNewPacienteMobile.addEventListener('click', e => {
-    if (!e.target.classList.contains('add_new_paciente_mobile_icon')) console.log(1);
+addNewLembretMobile.addEventListener('click', e => {
+    if (!e.target.classList.contains('add_new_paciente_mobile_icon')) novoLembrete.open()
 });
 
-addNewPacienteMobile.querySelector('.add_new_paciente_mobile_icon').addEventListener('click', e => {
+addNewLembretMobile.querySelector('.add_new_paciente_mobile_icon').addEventListener('click', e => {
     e.target.classList.toggle('icon_rotate');
-    addNewPacienteMobile.classList.toggle('active');
+    addNewLembretMobile.classList.toggle('active');
 });
 
 
@@ -56,10 +64,16 @@ window.onscroll = () => {
     if (prevPageYOfsset > currentPageYOfsset) {
         document.querySelector(".main_top_bar").style.top = "0";
         document.querySelector(".navigation_mobile").style.bottom = "0";
+        addNewLembretMobile.classList.remove('desactive');
+
     }
     else {
         document.querySelector(".main_top_bar").style.top = "-100px";
         document.querySelector(".navigation_mobile").style.bottom = "-100px";
+        addNewLembretMobile.classList.add('desactive');
+        addNewLembretMobile.classList.remove('active');
+        addNewLembretMobile.querySelector('.add_new_paciente_mobile_icon').classList.add('icon_rotate');
+
     }
     prevPageYOfsset = currentPageYOfsset;
 
@@ -67,7 +81,10 @@ window.onscroll = () => {
         setTimeout(() => {
             document.querySelector(".main_top_bar").style.top = "0";
             document.querySelector(".navigation_mobile").style.bottom = "0";
+            addNewLembretMobile.classList.remove('desactive');
         }, 1563);
+        addNewLembretMobile.querySelector('.add_new_paciente_mobile_icon').classList.remove('icon_rotate');
+
 
     }
 }
